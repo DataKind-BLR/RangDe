a77ef76eb9acfd9964d5bed17dac7de8d6b9ff80
Rishab Arora <rishab.arora@flipkart.com>
2015-12-06 02:19:57 +0530

    Putting CF methods into a class for easier management

--- Basanti/basanti.py
+++ Basanti/basanti.py
@@ -1,14 +1,27 @@
 from flask import Flask
 from flask import jsonify
 import recsys.cf as cf
+import yaml
 app = Flask(__name__)
+
+def fetch_configs():
+    with open("config.yaml", 'r') as stream:
+        configs = yaml.load(stream)
+        return configs
+
+cf_provider = cf.CachedCfRecommendation(fetch_configs())
 
 @app.route('/user/<userid>/recommendations')
 def serve_recommendation(userid):
-    recos = cf.fetch(userid)
+    recos = cf_provider.fetch(userid)
     reco_dict = { "borrower_ids" : recos }
     return jsonify(reco_dict)
 
 if __name__ == "__main__":
-    app.debug = True  # This is an internal API. This will come in handy.
+    app.debug = fetch_configs()["debug_mode"]
+    if app.debug is not True:   
+        import logging
+        from logging.handlers import RotatingFileHandler
+        file_handler = RotatingFileHandler('basanti.log', maxBytes=1024 * 1024 * 100, backupCount=20)
+        app.logger.addHandler(file_handler)
     app.run()
--- Basanti/recsys/cf.py
+++ Basanti/recsys/cf.py
@@ -3,18 +3,30 @@ import numpy as np
 from scipy.sparse import csr_matrix
 import pandas as pd
 from scipy import *
+import time
 
-db = MySQLdb.connect("localhost","root","root","rangde")
+class CachedCfRecommendation:
+    def refreshBorrowerVsOccupationFrame(self):
+        cursor = self.db.cursor()
+        cursor.execute('SELECT id, activity FROM rangde.loan_profiles;')
+        id_activities = cursor.fetchall()
+        id_list = [ seq[0] for seq in id_activities ]
+        activity_list = [ seq[1] for seq in id_activities ]
+        return pd.get_dummies(activity_list)
 
-def fetch(userid):
-    borrower_occupation_frame = getBorrowerVsOccupationFrame()
-    return ['42','43']
+    def getBorrowerVsOccupationFrame(self):
+        if (self.cached_frame is None or time.time() - self.cached_time > self.refresh_interval):
+            print "REFRESH"
+            self.cached_frame = self.refreshBorrowerVsOccupationFrame();
+        return self.cached_frame
 
+    def fetch(self, userid):
+        borrower_occupation_frame = self.getBorrowerVsOccupationFrame()
+        return ['42','43']
 
-def getBorrowerVsOccupationFrame():
-    cursor = db.cursor()
-    cursor.execute('SELECT id, activity FROM rangde.loan_profiles;')
-    id_activities = cursor.fetchall()
-    id_list = [ seq[0] for seq in id_activities ]
-    activity_list = [ seq[1] for seq in id_activities ]
-    return pd.get_dummies(activity_list)
+    def __init__(self, configs):
+        self.db = MySQLdb.connect("localhost","root","root","rangde")
+        self.refresh_interval = configs["refresh_interval"]
+        self.cached_frame = None
+        self.cached_time = time.time()
+    
